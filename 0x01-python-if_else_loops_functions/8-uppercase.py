84c389449d76bb842f2cd638e8fa82aa4e044618
Pibo Shongwe <piboman@yahoo.com>
2023-10-31 20:24:57 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def uppercase(c):
-    return ord('A') <= ord(c) <= ord('Z')
+def uppercase(str):
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            uppercase_char = char
+        print(uppercase_char, end='')
+    print()
