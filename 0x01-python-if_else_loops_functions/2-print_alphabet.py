3709e8bc855738532b129a20d0fab04111eba372
Pibo Shongwe <piboman@yahoo.com>
2023-10-31 19:35:55 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-output = ""
 for char_code in range(ord('a'), ord('z') + 1):
-    output += chr(char_code)
-print("{}".format(output), end="")
+    print("{}".format(chr(char_code)), end="")
