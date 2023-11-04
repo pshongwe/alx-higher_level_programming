51246ef29b20a3a132b036c5dfa1bb5e21fc40ee
Pibo Shongwe <piboman@yahoo.com>
2023-11-04 14:38:13 -0700

    u

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    n = len(matrix)
-    m = len(matrix[0])
-    for i in range(0, n):
-        for j in range(0, m):
-            if j != m - 1:
+    for i in matrix:
+        for j in i:
+            if j != i[-1]:
                 print("{}".format(matrix[i][j]), end=' ')
             else:
                 print("{}".format(matrix[i][j]), end='')
-        print("")
+        print()
