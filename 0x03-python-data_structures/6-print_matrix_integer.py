843d28b74d4401a83e15d7c91016c4b09baf12e9
Pibo Shongwe <piboman@yahoo.com>
2023-11-04 14:39:32 -0700

    u

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j != i[-1]:
-                print("{}".format(matrix[i][j]), end=' ')
+                print("{:d}".format(matrix[i][j]), end=' ')
             else:
-                print("{}".format(matrix[i][j]), end='')
+                print("{:d}".format(matrix[i][j]), end='')
         print()
