faf922f7bb7a78748070dc4a9acce34c12acaad2
Pibo Shongwe <piboman@yahoo.com>
2023-11-09 21:29:38 +0200

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,13 +5,15 @@
  * print_python_bytes - Print information about a Python bytes object.
  * @p: A Python object to be examined.
  */
-void print_python_bytes(PyObject *p) {
+void print_python_bytes(PyObject *p)
+{
 long int bytes, lim, x;
 char *buffer;
 
 printf("[.] bytes object info\n");
 
-if (!PyBytes_Check(p)) {
+if (!PyBytes_Check(p))
+{
 printf("  [ERROR] Invalid Bytes Object\n");
 return;
 }
@@ -25,7 +27,8 @@ printf("  trying string: %s\n", buffer);
 lim = (bytes >= 10) ? 10 : bytes + 1;
 printf("  first %ld bytes: ", lim);
 
-for (x = 0; x < lim; x++) {
+for (x = 0; x < lim; x++)
+{
 printf("%02x", (unsigned char)buffer[x]);
 if (x < lim - 1)
 printf(" ");
@@ -38,13 +41,15 @@ printf("\n");
  * print_python_list - Print information about a Python list object.
  * @p: A Python object to be examined.
  */
-void print_python_list(PyObject *p) {
+void print_python_list(PyObject *p)
+{
 long int x, size;
 PyObject *elem;
 
 printf("[*] Python list info\n");
 
-if (!PyList_Check(p)) {
+if (!PyList_Check(p))
+{
 printf("  [ERROR] Invalid List Object\n");
 return;
 }
@@ -54,7 +59,8 @@ size = ((PyVarObject *)(p))->ob_size;
 printf("[*] Size of the Python List = %ld\n", size);
 printf("[*] Allocated = %ld\n", ((PyListObject *)(p))->allocated);
 
-for (x = 0; x < size; x++) {
+for (x = 0; x < size; x++)
+{
 elem = ((PyListObject *)(p))->ob_item[x];
 
 printf("Element %ld: %s\n", x, elem->ob_type->tp_name);
