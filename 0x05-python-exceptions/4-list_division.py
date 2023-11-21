3b8cee9bc05c7e9339944c4333e914ea16e0d505
Pibo Shongwe <piboman@yahoo.com>
2023-11-20 19:39:18 -0800

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             division_result = 0
             print("division by 0")
-        except (TypeError, ValueError):
+        except TypeError:
             division_result = 0
             print("wrong type")
         except IndexError:
