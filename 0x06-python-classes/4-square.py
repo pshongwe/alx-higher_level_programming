c469b167e7199e9c63b8d4c0347f36a27c4f9f60
Pibo Shongwe <piboman@yahoo.com>
2023-11-21 16:56:36 -0800

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Define a square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Compute the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
