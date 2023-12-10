71167795af0905c0899f4ed09ed39abd46de4dfc
Pibo Shongwe <piboman@yahoo.com>
2023-12-09 18:46:30 -0800

    u

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" add item """
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+try:
+    if os.path.exists(filename):
+        items = load_from_json_file(filename)
+    else:
+        items = []
+except e:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
