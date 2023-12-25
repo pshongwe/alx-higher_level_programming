d09192bd98e81c93d096cde4378859eb54fbffb7
Pibo Shongwe <piboman@yahoo.com>
2023-12-24 19:06:57 -0800

    u

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -12,8 +12,18 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieve a dictionary representation of a Student instance."""
         obj = self.__dict__.copy()
+
         if not attrs:  # If attrs is None, return the whole dictionary
-            return obj
-        if not all(isinstance(attr, str) for attr in attrs):
-            return obj
-        return {attr: obj[attr] for attr in attrs if attr in obj}
+            sorted_obj = dict(sorted(
+                obj.items(),
+                key=lambda x: ['age', 'last_name', 'first_name'].index(x[0])))
+            return sorted_obj
+
+        result = {}
+        sorting_order = ['age', 'last_name', 'first_name']
+
+        for attr in sorting_order:
+            if attr in attrs and attr in obj:
+                result[attr] = obj[attr]
+        return result
+
