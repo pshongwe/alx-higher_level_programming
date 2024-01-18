b322969a375d2750cea3fc0a9f19e440be4aa633
Pibo Shongwe <piboman@yahoo.com>
2024-01-18 15:03:51 -0800

    u

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,6 +8,9 @@ def matrix_divided(matrix, div):
     """
     divides all elements of a matrix.
     """
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
     m = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list) or \
        not all(isinstance(row, list) for row in matrix):
@@ -19,13 +22,13 @@ def matrix_divided(matrix, div):
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
     divided_matrix = []
     for row in matrix:
         divided_row = []
         for element in row:
+            if not isinstance(element, float) or \
+               not isinstance(element, int):
+                raise TypeError(m)
             result = round(element / div, 2)
             divided_row.append(result)
         divided_matrix.append(divided_row)
