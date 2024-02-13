b5a9873f80b81730a398367529bf7dace817927c
Pibo Shongwe <piboman@yahoo.com>
2024-02-12 19:21:50 -0800

    u

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const noArgs = 'No argument';
+const oneArg = 'Argument found';
+const args = 'Arguments found';
+if (process.argv.length === 2)
+{
+	console.log(noArgs);
+}
+else if(process.argv.length == 3)
+{
+	console.log(oneArg);
+}
+else
+{
+	console.log(args);
+}
