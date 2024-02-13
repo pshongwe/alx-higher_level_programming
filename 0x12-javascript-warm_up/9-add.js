edf039eb0949ce1c410d45204a4af7fa75621b5c
Pibo Shongwe <piboman@yahoo.com>
2024-02-12 19:44:19 -0800

    u

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+if (isNaN(process.argv[2]) || isNaN(process.argv[3]) || process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log('NaN');
+} else {
+  const num1 = parseInt(process.argv[2]);
+  const num2 = parseInt(process.argv[3]);
+  console.log(add(num1, num2));
+}
