15edf45e9f8f556a7b7033c2fd01d6e2d5f33840
Pibo Shongwe <piboman@yahoo.com>
2024-02-18 01:56:09 -0800

    u

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists filtered states from database"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    h = "localhost"
+    u = sys.argv[1]
+    pswd = sys.argv[2]
+    d = sys.argv[3]
+    p = 3306
+    db = MySQLdb.connect(host=h, user=u, passwd=pswd, db=d, port=p)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states where name like 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
