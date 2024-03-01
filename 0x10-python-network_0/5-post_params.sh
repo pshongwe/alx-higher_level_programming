6312a8e476e5678d3a55abdf39f269cf5f3e1348
Pibo Shongwe <piboman@yahoo.com>
2024-02-29 19:53:27 -0800

    u

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in list_of_integers"""
+    if not list_of_integers:
+        return None
+
+    lo = 0
+    hi = len(list_of_integers) - 1
+
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            lo = mid + 1
+        else:
+            hi = mid
+
+    return list_of_integers[lo]
