3d86814ab7787bd231d306d12676a8e6df1b6131
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 11:02:32 -0800

    u

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const fs = require('fs');
+const [,, fileA, fileB, fileC] = process.argv;
+const contentA = fs.readFileSync(fileA, 'utf8');
+const contentB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, contentA + contentB);
