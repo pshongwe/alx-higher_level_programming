1e6677ee2b394013cf15bd2451440c3ad42d267e
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 00:51:28 -0800

    u

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import urllib.request
 """call url"""
 
 
 if __name__ == "__main__":
+    import urllib.request
+
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
