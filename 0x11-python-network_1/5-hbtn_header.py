0e87f5e1f56a48368e612327f26828362a645778
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 09:27:21 -0800

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Fetches the value of the X-Request-Id variable from a response header."""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
