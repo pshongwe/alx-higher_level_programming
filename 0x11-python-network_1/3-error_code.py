5fda0ccc8e145a26c3020e3fc6f802819795faaf
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 09:19:29 -0800

    u

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import urllib.request
+import urllib.error
+import sys
+"""make request"""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
