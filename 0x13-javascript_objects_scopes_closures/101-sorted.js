93b4e25ab3beccb684c7befc6d51e23658d78474
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 11:01:22 -0800

    u

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { dict } = require('./101-data.js');
+const newDict = {};
+Object.entries(dict).forEach(([userId, occurrence]) => {
+  if (!newDict[occurrence]) {
+    newDict[occurrence] = [];
+  }
+  newDict[occurrence].push(userId);
+});
+console.log(newDict);
