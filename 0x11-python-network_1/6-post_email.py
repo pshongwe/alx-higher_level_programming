289a886d8c38b01a47dd1dc0127ad14e57830feb
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 09:28:52 -0800

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Sends a POST request with an email parameter
+and displays the response body."""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
