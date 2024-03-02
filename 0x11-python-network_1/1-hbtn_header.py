687b069e1407e4927497b8575eb93d5d79a042c2
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 01:09:51 -0800

    u

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""call url"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.info().get('X-Request-Id'))
