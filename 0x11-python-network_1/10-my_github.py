8ae9b7a19e66e65c01edd527d592f09acbd8455a
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 09:35:12 -0800

    u

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""make a request to github
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
