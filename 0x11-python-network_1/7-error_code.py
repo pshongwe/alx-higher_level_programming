15d1dd400568e75b6c52f751723fe1750eba441e
Pibo Shongwe <piboman@yahoo.com>
2024-03-02 09:30:00 -0800

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Sends a request to a URL and displays the body
+of the response or an error code."""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
