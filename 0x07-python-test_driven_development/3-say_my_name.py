da2bcc9056513ba9330369b6bbb39d0e5b47f3db
Pibo Shongwe <piboman@yahoo.com>
2024-03-22 12:50:52 -0700

    u

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """say my name function"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
