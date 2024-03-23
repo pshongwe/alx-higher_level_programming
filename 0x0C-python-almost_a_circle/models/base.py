ea64ac4f985201baf3d62a0efd2fbe28daf8923d
Pibo Shongwe <piboman@yahoo.com>
2024-03-23 09:18:37 -0700

    u

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,15 @@ class Base:
         if list_dictionaries is None:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
+        filename = f"{cls.__name__}.json"
+        if list_objs:
+            list_dictionaries = [obj.to_dictionary() for obj in list_objs]
+        else:
+            list_dictionaries = []
+        json_string = cls.to_json_string(list_dictionaries)
+        with open(filename, 'w') as file:
+            file.write(json_string)
