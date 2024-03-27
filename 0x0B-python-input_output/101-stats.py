9ed253d1a5464328ff2da357528981fd4c4c01ab
Pibo Shongwe <piboman@yahoo.com>
2024-03-27 09:30:32 -0700

    u

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_stats(total_size, status_codes):
+    """print stats"""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
+
+
+def main():
+    """main"""
+    total_size = 0
+    status_codes = {}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            data = line.split()
+            if len(data) < 2:
+                continue
+
+            size = int(data[-1])
+            status_code = data[-2]
+
+            total_size += size
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
+
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
+
+    print_stats(total_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
