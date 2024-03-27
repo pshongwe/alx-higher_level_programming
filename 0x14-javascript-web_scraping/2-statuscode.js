97a7ded7231937d07cdea99bf195fceecd8b53ed
Pibo Shongwe <piboman@yahoo.com>
2024-03-26 19:18:12 -0700

    task2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if the required arguments are provided
+if (process.argv.length < 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write the string to the file
+fs.writeFile(filePath, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
