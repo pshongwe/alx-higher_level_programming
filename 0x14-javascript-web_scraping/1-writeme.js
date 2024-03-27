0658e4dedef33f7e7d22c06c0f2eb5fc3f883ec0
Pibo Shongwe <piboman@yahoo.com>
2024-03-26 19:16:05 -0700

    task1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  console.error('Usage: ./1-writeme.js <file_path> "<string>"');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
