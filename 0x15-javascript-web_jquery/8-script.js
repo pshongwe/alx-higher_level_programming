02d9d13c36bf3ce158b60b681053c69258ba6689
Pibo Shongwe <piboman@yahoo.com>
2024-04-03 23:38:06 +0200

    u

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+    data.results.forEach((movie) => {
+        $('UL#list_movies').append('<li>' + movie.title + '</li>'); 
+    });
+});
